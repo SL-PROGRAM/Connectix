1d2a7e82f3709716395d55cee9f1d718b0bd1a21
sl-prog <s.leyral@gmail.com>
2019-03-06 15:58:10 +0100

    add entity administration extends HumanRessource

---
+++ Connectix/src/Entity/Administration.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AdministrationRepository")
+ */
+class Administration extends HumanRessource
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $administationActivity;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAdministationActivity(): ?int
+    {
+        return $this->administationActivity;
+    }
+
+    public function setAdministationActivity(int $administationActivity): self
+    {
+        $this->administationActivity = $administationActivity;
+
+        return $this;
+    }
+}
