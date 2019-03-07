6e21112516987816acb6d2d2627dbe94a4e11903
sl-prog <s.leyral@gmail.com>
2019-03-07 11:46:51 +0100

    modify entity ProductionCadre to  ProductionCadre extends Production

--- Connectix/src/Entity/ProductionCadre.php
+++ Connectix/src/Entity/ProductionCadre.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProductionCadreRepository")
  */
-abstract class ProductionCadre
+abstract class ProductionCadre extends Production
 {
     /**
      * @ORM\Id()
