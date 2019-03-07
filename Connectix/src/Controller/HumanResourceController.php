eacc3485186bef9507b5b02aa556eb5abe0e2f8d
sl-prog <s.leyral@gmail.com>
2019-03-07 11:47:36 +0100

    modify entity HumanResources
    add @ORM\InheritanceType("JOINED")

--- Connectix/src/Controller/HumanResourceController.php
+++ Connectix/src/Controller/HumanResourceController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Game;
+use App\Entity\Socity;
 use App\Entity\Technician;
+use App\Entity\HumanResource;
+use App\Entity\WorkMan;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,28 +17,95 @@ class HumanResourceController extends AbstractController
      */
     public function index()
     {
+        $this->makeGame();
+        $socity = $this->makeSocity();
+        $this->makeTechnician($socity);
+
         return $this->render('human_resource/index.html.twig', [
             'controller_name' => 'HumanResourceController',
         ]);
     }
 
-    public function makeTechnician($salary, $formation, $exprience, $productivity, $administrationActivityCost, $coeficientSalary, $productionActivity, $administationActivity){
+    public function makeGame(){
         $entityManager = $this->getDoctrine()->getManager();
+        $game = new Game();
+
+        $game   ->setName('bob')
+                ->setCreatAt(new \DateTime())
+                ->setMaxturn(20)
+                ->setSmic(1500)
+                ->setSocityNumber(5)
+                ->setTurn(0)
+                ->setTva(20);
+        $entityManager  ->persist($game);
+        $entityManager  ->flush();
+    }
+
+
+    public function makeSocity(){
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $socity = new Socity();
+
+        $socity ->setName('bob')
+                ->setMoneyStart(10000)
+                ->setPriceMaxPublicityImpact(100000)
+                ->setPriceMinPublicicyImpact(1000);
+
+        $entityManager  ->persist($socity);
+        $entityManager  ->flush();
+
+        return $socity;
+    }
+
+    public function makeTechnician(Socity $socity, $administrationActivity, $productionActivity, $salary, $formation, $productivity, $experience, $administrationActivityCost, $coeficientSalary){
+
+        $entityManager = $this->getDoctrine()->getManager();
+
 
         $technician = new Technician();
 
-        $technician     ->setAdministrationActivity(10)
-                        ->setSalary(1000)
-                        ->setFormation(100)
-                        ->setProductivity(100)
-                        ->setExprience(2)
-                        ->setAdministrationActivityCost(100)
-                        ->setCoeficientSalary(1.2)
-                        ->setProductionActivity(100);
+        $technician     ->setProductionActivity($productionActivity)
+                        ->setAdministrationActivity($administrationActivity)
+                        ->setSalary($salary)
+                        ->setFormation($formation)
+                        ->setProductivity($productivity)
+                        ->setExprience($experience)
+                        ->setAdministrationActivityCost($administrationActivityCost)
+                        ->setCoeficientSalary($coeficientSalary)
+                        ->setSocity($socity);
+
 
         $entityManager  ->persist($technician);
         $entityManager  ->flush();
 
     }
 
+    public function makeWorMan(Socity $socity, $productionActivity, $salary, $formation, $productivity, $experience, $administrationActivityCost, $coeficientSalary){
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+
+        $technician = new WorkMan();
+
+        $technician     ->setProductionActivity($productionActivity)
+                        ->setSalary($salary)
+                        ->setFormation($formation)
+                        ->setProductivity($productivity)
+                        ->setExprience($experience)
+                        ->setAdministrationActivityCost($administrationActivityCost)
+                        ->setCoeficientSalary($coeficientSalary)
+                        ->setSocity($socity);
+
+
+        $entityManager  ->persist($technician);
+        $entityManager  ->flush();
+
+    }
+
+
+
+
 }
+
+
--- Connectix/src/Entity/HumanResource.php
+++ Connectix/src/Entity/HumanResource.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\HumanRessourceRepository")
+ * @ORM\InheritanceType("JOINED")
  */
 abstract class HumanResource
 {
