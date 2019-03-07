625a5e3afb809697402e8474987cc8cfeb6db5d9
sl-prog <s.leyral@gmail.com>
2019-03-07 12:32:00 +0100

    modify Game entity to add variable game parameter:
        salesPriceMinLvl1
        salesPriceMaxLvl1
        salesPriceMinLvl2
        salesPriceMinLvl3
        salesPriceMinLvl4
        salesPriceMaxLvl2
        salesPriceMaxLvl3
        salesPriceMaxLvl4
        productNumberMinLvl1
        productNumberMinLvl2
        productNumberMinLvl3
        productNumberMinLvl4
        productNumberMaxLvl1
        productNumberMaxLvl2
        productNumberMaxLvl3
        productNumberMaxLvl4
        percentProductAvailableMinCycleLife1
        percentProductAvailableMinCycleLife2
        percentProductAvailableMinCycleLife3
        percentProductAvailableMinCycleLife4
        percentProductAvailableMaxCycleLife1
        percentProductAvailableMaxCycleLife2
        percentProductAvailableMaxCycleLife3
        percentProductAvailableMaxCycleLife4
        productQualityMinCycleLife1
        productQualityMinCycleLife2
        productQualityMinCycleLife3
        productQualityMinCycleLife4
        productQualityMaxCycleLife1
        productQualityMaxCycleLife2
        productQualityMaxCycleLife3
        productQualityMaxCycleLife4

--- Connectix/src/Entity/Game.php
+++ Connectix/src/Entity/Game.php
@@ -64,6 +64,166 @@ class Game
     private $socities;
 
     /**
+     * @ORM\Column(type="integer")
+     */
+    private $salesPriceMinLvl1;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $salesPriceMaxLvl1;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $salesPriceMinLvl2;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $salesPriceMinLvl3;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $salesPriceMinLvl4;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $salesPriceMaxLvl2;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $salesPriceMaxLvl3;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $salesPriceMaxLvl4;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productNumberMinLvl1;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productNumberMinLvl2;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productNumberMinLvl3;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productNumberMinLvl4;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productNumberMaxLvl1;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productNumberMaxLvl2;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productNumberMaxLvl3;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productNumberMaxLvl4;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $percentProductAvailableMinCycleLife1;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $percentProductAvailableMinCycleLife2;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $percentProductAvailableMinCycleLife3;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $percentProductAvailableMinCycleLife4;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $percentProductAvailableMaxCycleLife1;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $percentProductAvailableMaxCycleLife2;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $percentProductAvailableMaxCycleLife3;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $percentProductAvailableMaxCycleLife4;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productQualityMinCycleLife1;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productQualityMinCycleLife2;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productQualityMinCycleLife3;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productQualityMinCycleLife4;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productQualityMaxCycleLife1;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productQualityMaxCycleLife2;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productQualityMaxCycleLife3;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productQualityMaxCycleLife4;
+
+    /**
      * Game constructor.
      */
     public function __construct()
@@ -287,6 +447,390 @@ class Game
                 $socity->setGame(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getSalesPriceMinLvl1(): ?int
+    {
+        return $this->salesPriceMinLvl1;
+    }
+
+    public function setSalesPriceMinLvl1(int $salesPriceMinLvl1): self
+    {
+        $this->salesPriceMinLvl1 = $salesPriceMinLvl1;
+
+        return $this;
+    }
+
+    public function getSalesPriceMaxLvl1(): ?int
+    {
+        return $this->salesPriceMaxLvl1;
+    }
+
+    public function setSalesPriceMaxLvl1(int $salesPriceMaxLvl1): self
+    {
+        $this->salesPriceMaxLvl1 = $salesPriceMaxLvl1;
+
+        return $this;
+    }
+
+    public function getSalesPriceMinLvl2(): ?int
+    {
+        return $this->salesPriceMinLvl2;
+    }
+
+    public function setSalesPriceMinLvl2(int $salesPriceMinLvl2): self
+    {
+        $this->salesPriceMinLvl2 = $salesPriceMinLvl2;
+
+        return $this;
+    }
+
+    public function getSalesPriceMinLvl3(): ?int
+    {
+        return $this->salesPriceMinLvl3;
+    }
+
+    public function setSalesPriceMinLvl3(int $salesPriceMinLvl3): self
+    {
+        $this->salesPriceMinLvl3 = $salesPriceMinLvl3;
+
+        return $this;
+    }
+
+    public function getSalesPriceMinLvl4(): ?int
+    {
+        return $this->salesPriceMinLvl4;
+    }
+
+    public function setSalesPriceMinLvl4(int $salesPriceMinLvl4): self
+    {
+        $this->salesPriceMinLvl4 = $salesPriceMinLvl4;
+
+        return $this;
+    }
+
+    public function getSalesPriceMaxLvl2(): ?int
+    {
+        return $this->salesPriceMaxLvl2;
+    }
+
+    public function setSalesPriceMaxLvl2(int $salesPriceMaxLvl2): self
+    {
+        $this->salesPriceMaxLvl2 = $salesPriceMaxLvl2;
+
+        return $this;
+    }
+
+    public function getSalesPriceMaxLvl3(): ?int
+    {
+        return $this->salesPriceMaxLvl3;
+    }
+
+    public function setSalesPriceMaxLvl3(int $salesPriceMaxLvl3): self
+    {
+        $this->salesPriceMaxLvl3 = $salesPriceMaxLvl3;
+
+        return $this;
+    }
+
+    public function getSalesPriceMaxLvl4(): ?int
+    {
+        return $this->salesPriceMaxLvl4;
+    }
+
+    public function setSalesPriceMaxLvl4(int $salesPriceMaxLvl4): self
+    {
+        $this->salesPriceMaxLvl4 = $salesPriceMaxLvl4;
+
+        return $this;
+    }
+
+    public function getProductNumberMinLvl1(): ?int
+    {
+        return $this->productNumberMinLvl1;
+    }
+
+    public function setProductNumberMinLvl1(int $productNumberMinLvl1): self
+    {
+        $this->productNumberMinLvl1 = $productNumberMinLvl1;
+
+        return $this;
+    }
+
+    public function getProductNumberMinLvl2(): ?int
+    {
+        return $this->productNumberMinLvl2;
+    }
+
+    public function setProductNumberMinLvl2(int $productNumberMinLvl2): self
+    {
+        $this->productNumberMinLvl2 = $productNumberMinLvl2;
+
+        return $this;
+    }
+
+    public function getProductNumberMinLvl3(): ?int
+    {
+        return $this->productNumberMinLvl3;
+    }
+
+    public function setProductNumberMinLvl3(int $productNumberMinLvl3): self
+    {
+        $this->productNumberMinLvl3 = $productNumberMinLvl3;
+
+        return $this;
+    }
+
+    public function getProductNumberMinLvl4(): ?int
+    {
+        return $this->productNumberMinLvl4;
+    }
+
+    public function setProductNumberMinLvl4(int $productNumberMinLvl4): self
+    {
+        $this->productNumberMinLvl4 = $productNumberMinLvl4;
+
+        return $this;
+    }
+
+    public function getProductNumberMaxLvl1(): ?int
+    {
+        return $this->productNumberMaxLvl1;
+    }
+
+    public function setProductNumberMaxLvl1(int $productNumberMaxLvl1): self
+    {
+        $this->productNumberMaxLvl1 = $productNumberMaxLvl1;
+
+        return $this;
+    }
+
+    public function getProductNumberMaxLvl2(): ?int
+    {
+        return $this->productNumberMaxLvl2;
+    }
+
+    public function setProductNumberMaxLvl2(int $productNumberMaxLvl2): self
+    {
+        $this->productNumberMaxLvl2 = $productNumberMaxLvl2;
+
+        return $this;
+    }
+
+    public function getProductNumberMaxLvl3(): ?int
+    {
+        return $this->productNumberMaxLvl3;
+    }
+
+    public function setProductNumberMaxLvl3(int $productNumberMaxLvl3): self
+    {
+        $this->productNumberMaxLvl3 = $productNumberMaxLvl3;
+
+        return $this;
+    }
+
+    public function getProductNumberMaxLvl4(): ?int
+    {
+        return $this->productNumberMaxLvl4;
+    }
+
+    public function setProductNumberMaxLvl4(int $productNumberMaxLvl4): self
+    {
+        $this->productNumberMaxLvl4 = $productNumberMaxLvl4;
+
+        return $this;
+    }
+
+    public function getPercentProductAvailableMinCycleLife1(): ?int
+    {
+        return $this->percentProductAvailableMinCycleLife1;
+    }
+
+    public function setPercentProductAvailableMinCycleLife1(int $percentProductAvailableMinCycleLife1): self
+    {
+        $this->percentProductAvailableMinCycleLife1 = $percentProductAvailableMinCycleLife1;
+
+        return $this;
+    }
+
+    public function getPercentProductAvailableMinCycleLife2(): ?int
+    {
+        return $this->percentProductAvailableMinCycleLife2;
+    }
+
+    public function setPercentProductAvailableMinCycleLife2(int $percentProductAvailableMinCycleLife2): self
+    {
+        $this->percentProductAvailableMinCycleLife2 = $percentProductAvailableMinCycleLife2;
+
+        return $this;
+    }
+
+    public function getPercentProductAvailableMinCycleLife3(): ?int
+    {
+        return $this->percentProductAvailableMinCycleLife3;
+    }
+
+    public function setPercentProductAvailableMinCycleLife3(int $percentProductAvailableMinCycleLife3): self
+    {
+        $this->percentProductAvailableMinCycleLife3 = $percentProductAvailableMinCycleLife3;
+
+        return $this;
+    }
+
+    public function getPercentProductAvailableMinCycleLife4(): ?int
+    {
+        return $this->percentProductAvailableMinCycleLife4;
+    }
+
+    public function setPercentProductAvailableMinCycleLife4(int $percentProductAvailableMinCycleLife4): self
+    {
+        $this->percentProductAvailableMinCycleLife4 = $percentProductAvailableMinCycleLife4;
+
+        return $this;
+    }
+
+    public function getPercentProductAvailableMaxCycleLife1(): ?int
+    {
+        return $this->percentProductAvailableMaxCycleLife1;
+    }
+
+    public function setPercentProductAvailableMaxCycleLife1(int $percentProductAvailableMaxCycleLife1): self
+    {
+        $this->percentProductAvailableMaxCycleLife1 = $percentProductAvailableMaxCycleLife1;
+
+        return $this;
+    }
+
+    public function getPercentProductAvailableMaxCycleLife2(): ?int
+    {
+        return $this->percentProductAvailableMaxCycleLife2;
+    }
+
+    public function setPercentProductAvailableMaxCycleLife2(int $percentProductAvailableMaxCycleLife2): self
+    {
+        $this->percentProductAvailableMaxCycleLife2 = $percentProductAvailableMaxCycleLife2;
+
+        return $this;
+    }
+
+    public function getPercentProductAvailableMaxCycleLife3(): ?int
+    {
+        return $this->percentProductAvailableMaxCycleLife3;
+    }
+
+    public function setPercentProductAvailableMaxCycleLife3(int $percentProductAvailableMaxCycleLife3): self
+    {
+        $this->percentProductAvailableMaxCycleLife3 = $percentProductAvailableMaxCycleLife3;
+
+        return $this;
+    }
+
+    public function getPercentProductAvailableMaxCycleLife4(): ?int
+    {
+        return $this->percentProductAvailableMaxCycleLife4;
+    }
+
+    public function setPercentProductAvailableMaxCycleLife4(int $percentProductAvailableMaxCycleLife4): self
+    {
+        $this->percentProductAvailableMaxCycleLife4 = $percentProductAvailableMaxCycleLife4;
+
+        return $this;
+    }
+
+    public function getProductQualityMinCycleLife1(): ?int
+    {
+        return $this->productQualityMinCycleLife1;
+    }
+
+    public function setProductQualityMinCycleLife1(int $productQualityMinCycleLife1): self
+    {
+        $this->productQualityMinCycleLife1 = $productQualityMinCycleLife1;
+
+        return $this;
+    }
+
+    public function getProductQualityMinCycleLife2(): ?int
+    {
+        return $this->productQualityMinCycleLife2;
+    }
+
+    public function setProductQualityMinCycleLife2(int $productQualityMinCycleLife2): self
+    {
+        $this->productQualityMinCycleLife2 = $productQualityMinCycleLife2;
+
+        return $this;
+    }
+
+    public function getProductQualityMinCycleLife3(): ?int
+    {
+        return $this->productQualityMinCycleLife3;
+    }
+
+    public function setProductQualityMinCycleLife3(int $productQualityMinCycleLife3): self
+    {
+        $this->productQualityMinCycleLife3 = $productQualityMinCycleLife3;
+
+        return $this;
+    }
+
+    public function getProductQualityMinCycleLife4(): ?int
+    {
+        return $this->productQualityMinCycleLife4;
+    }
+
+    public function setProductQualityMinCycleLife4(int $productQualityMinCycleLife4): self
+    {
+        $this->productQualityMinCycleLife4 = $productQualityMinCycleLife4;
+
+        return $this;
+    }
+
+    public function getProductQualityMaxCycleLife1(): ?int
+    {
+        return $this->productQualityMaxCycleLife1;
+    }
+
+    public function setProductQualityMaxCycleLife1(int $productQualityMaxCycleLife1): self
+    {
+        $this->productQualityMaxCycleLife1 = $productQualityMaxCycleLife1;
+
+        return $this;
+    }
+
+    public function getProductQualityMaxCycleLife2(): ?int
+    {
+        return $this->productQualityMaxCycleLife2;
+    }
+
+    public function setProductQualityMaxCycleLife2(int $productQualityMaxCycleLife2): self
+    {
+        $this->productQualityMaxCycleLife2 = $productQualityMaxCycleLife2;
+
+        return $this;
+    }
+
+    public function getProductQualityMaxCycleLife3(): ?int
+    {
+        return $this->productQualityMaxCycleLife3;
+    }
+
+    public function setProductQualityMaxCycleLife3(int $productQualityMaxCycleLife3): self
+    {
+        $this->productQualityMaxCycleLife3 = $productQualityMaxCycleLife3;
+
+        return $this;
+    }
+
+    public function getProductQualityMaxCycleLife4(): ?int
+    {
+        return $this->productQualityMaxCycleLife4;
+    }
+
+    public function setProductQualityMaxCycleLife4(int $productQualityMaxCycleLife4): self
+    {
+        $this->productQualityMaxCycleLife4 = $productQualityMaxCycleLife4;
 
         return $this;
     }
