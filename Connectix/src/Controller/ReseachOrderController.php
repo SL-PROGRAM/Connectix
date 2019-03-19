ed25076eb2c9742c1316d0f989a6eb74c28dfa53
sl-prog <s.leyral@gmail.com>
2019-03-19 18:03:00 +0100

    modify route

--- Connectix/src/Controller/ReseachOrderController.php
+++ Connectix/src/Controller/ReseachOrderController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/reseach/order")
+ * @Route("/reseachorder")
  */
 class ReseachOrderController extends AbstractController
 {
