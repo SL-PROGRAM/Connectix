9078a97e6826816ba83f22f2347b74eaf4458489
sl-prog <s.leyral@gmail.com>
2019-03-19 09:17:01 +0100

    add route to new loan by duration and dueDate

--- Connectix/src/Controller/LoanController.php
+++ Connectix/src/Controller/LoanController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Loan;
+use App\Form\LoanByDueDateType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LoanController extends AbstractController
@@ -16,4 +20,77 @@ class LoanController extends AbstractController
             'controller_name' => 'LoanController',
         ]);
     }
+
+    /**
+     * @Route("/new/duedate", name="socity_new", methods={"GET","POST"})
+     */
+    public function newLoanByDueDate(Request $request): Response
+    {
+        $loan = new Loan();
+        $form = $this->createForm(LoanByDueDateType::class, $loan);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //TODO add calculation of loanDuration
+            $loanDuration = 0;
+            //TODO add get to bankInterest
+            $bankInterest = 0;
+
+            $socity = $this->getUser()->getSocity();
+
+
+            $loan   ->setSocity($socity)
+                    ->setBankInterest($bankInterest)
+                    ->setLoanDuration($loanDuration);
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($loan);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('loan');
+        }
+
+        return $this->render('loan/new.html.twig', [
+            'loan' => $loan,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/new/duration", name="socity_new", methods={"GET","POST"})
+     */
+    public function newLoanByDuration(Request $request): Response
+    {
+        $loan = new Loan();
+        $form = $this->createForm(LoanByDueDateType::class, $loan);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //TODO add calculation of loanDueDate
+            $loanDueDate = 0;
+            //TODO add get to bankInterest
+            $bankInterest = 0;
+
+            $socity = $this->getUser()->getSocity();
+
+
+            $loan   ->setSocity($socity)
+                    ->setBankInterest($bankInterest)
+                    ->setMonthlyDueDate($loanDueDate);
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($loan);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('loan');
+        }
+
+        return $this->render('loan/new.html.twig', [
+            'loan' => $loan,
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
